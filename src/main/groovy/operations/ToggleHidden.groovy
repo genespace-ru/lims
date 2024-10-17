f4d329f1f0c61192d0bbbdb782c3fc039fdb7160
Sergey Zhatchenko <zha@DevelopmentOnTheEdge.com>
2024-10-17 16:28:12 +0700

    Toggle operation stubs - 2 more

--- src/main/groovy/operations/ToggleHidden.groovy
+++ src/main/groovy/operations/ToggleHidden.groovy
@@ -2,13 +2,13 @@ package operations
 
 import com.developmentontheedge.be5.server.operations.support.GOperationSupport
 
-public class ToogleAttached extends GOperationSupport
+public class ToggleHidden extends GOperationSupport
 {
     @Override
     public void invoke(Object parameters) throws Exception
     {
         db.update( """UPDATE snv_sample1 
-                        SET isAttached = CASE WHEN isAttached = 'yes' THEN 'no' ELSE 'yes' END 
+                        SET isHidden = CASE WHEN isHidden = 'yes' THEN 'no' ELSE 'yes' END 
                       WHERE ID = ?""", 
             context.params.ID as Long )        
 
