0a6c8adab0fdce737b615f71cf92d6c3d79867a4
anna <anna@developmentontheedge.com>
2025-07-14 13:46:59 +0700

    Add archived fasta, manage errors

--- src/main/groovy/operations/LoadProject.groovy
+++ src/main/groovy/operations/LoadProject.groovy
@@ -18,6 +18,7 @@ import ru.biosoft.access.core.DataCollection
 import ru.biosoft.access.core.DataElement
 import ru.biosoft.access.core.DataElementImporter
 import ru.biosoft.access.file.GenericFileDataCollection
+import ru.biosoft.access.file.FileTypeRegistry
 import ru.biosoft.jobcontrol.FunctionJobControl
 import ru.biosoft.jobcontrol.JobControl
 import ru.biosoft.jobcontrol.SubFunctionJobControl
@@ -85,13 +86,7 @@ public class LoadProject extends GOperationSupport {
         DynamicProperty prop = new DynamicProperty("sequencesFile", "Sequences (fastg.gz or fastq files)", java.io.File.class);
         prop.setAttribute(BeanInfoConstants.MULTIPLE_SELECTION_LIST, true )
         params.add(prop)
-        dpsHelper.addLabelRaw(params, "Single or multiple files with *.fastq or *.fastq.gz (archives with multiple fasta are NOT allowed)")
-        //TODO: !!!! архив архивов или отдельные файлы?
-        //Архив fastq.gzip - архив с последовательностями
-
-        //        params.sequencesFile = [ value: null, TYPE: java.io.File,
-        //            DISPLAY_NAME: "Sequences (.tar.gz archive)",
-        //            MULTIPLE_SELECTION_LIST: "true" ]
+        dpsHelper.addLabelRaw(params, "Single or multiple files with *.fastq or *.fastq.gz (.tar, .tar.gz, zip archive with multiple fasta is allowed)")
 
         params = DpsUtils.setValues(params, presetValues)
 
@@ -143,6 +138,7 @@ public class LoadProject extends GOperationSupport {
             return
         }
 
+        List<String> errors = new ArrayList<>()
         def description = params.$description
         def projectId = database.projects << [name: projectName, description: description]
 
@@ -158,7 +154,8 @@ public class LoadProject extends GOperationSupport {
             IOUtils.copy(source, destination);
         }
         catch( Exception e ) {
-            throw new NullPointerException("Error copy sampleSheet file: " + e.getMessage());
+            errors.add("Error copy sampleSheet file: " + e.getMessage() )
+            sampleSheetFile = null
         }
         finally {
             source.close();
@@ -187,27 +184,22 @@ public class LoadProject extends GOperationSupport {
                     source.close();
                     destination.close();
                 }
-                //TODO:tar and tar.gz are unpacked incorrectly if fastq.gz is inside
-                //int numInArchive = ArchiveFactory.getCount(seqfileTmp)
-                //if(numInArchive >1) {
-                //repo.importArchiveFile(seqfileTmp, samples)
-                //                }
-                //                else {
-                //single fasta, copy as is
-                ApplicationUtils.copyFile(repo.getChildFile(samples, seqfile.name ), seqfileTmp )
-                //}
+                if(ArchiveFactory.isComplexArchive(seqfileTmp)) {
+                    repo.importArchiveFile(seqfileTmp, samples)
+                }
+                else {
+                    //single fasta, copy as is
+                    ApplicationUtils.copyFile(repo.getChildFile(samples, seqfile.name ), seqfileTmp )
+                }
             }catch(Exception e) {
-                setResult(OperationResult.error("Error processing Sequence file: " + e.getMessage()))
+                errors.add("Error processing sequence file(s): " + e.getMessage())
             }
         }
         else
             for(int i = 0; i < files.length; i++) {
                 seqfile = getFileItem( files[i] )
                 try {
-                    File tempDir = TempFiles.getTempDirectory();
-                    //File seqfileTmp = new File(tempDir, seqfile.getName())
                     File seqfileTmp = repo.getChildFile(samples, seqfile.name );
-
                     source = seqfile.getInputStream();
                     destination = new FileOutputStream(seqfileTmp)
                     try {
@@ -217,9 +209,8 @@ public class LoadProject extends GOperationSupport {
                         source.close();
                         destination.close();
                     }
-                    //namesNoExt[seqfile.name.replaceFirst(~/\.[^\.]+$/, '')] = seqfile
                 }catch(Exception e) {
-                    setResult(OperationResult.error("Error processing Sequence file: " + e.getMessage()))
+                    errors.add("Error processing sequence file(s): " + e.getMessage())
                 }
             }
         for (String name: samples.getNameList()) {
@@ -227,16 +218,36 @@ public class LoadProject extends GOperationSupport {
             namesNoExt[deFile.getName().replaceFirst(~/\..+$/, '')] = deFile;
         }
 
+        //Sample sheet file is null or incorrect, do not add run info to database
+        if(sampleSheetFile == null) {
+            errors.add("Sample sheet file is not valid, no run information is available")
+            setResult(OperationResult.finished(String.join("\n", errors)))
+            return
+        }
+
         String sampleSheetFormat = params.$sampleSheetFormat
-        Object sampleSheetFormat2 = params.sampleSheetFormat
         String runName = sampleSheetFormat + " run";
         Map<String, Object> runInfo;
+
         if(sampleSheetFormat.equals(ILLUMINA_SAMPLE_SHEET )) {
-            runInfo = IlluminaCSVParser.parseFile(sampleSheetFile)
+            try {
+                runInfo = IlluminaCSVParser.parseFile(sampleSheetFile)
+            }
+            catch(Exception e) {
+                errors.add("Error parsing sample sheet file: " + e.getMessage())
+            }
         }
         else if(sampleSheetFormat.equals(YAML_SAMPLE_SHEET)) {
-            runInfo = YamlParser.parseFile(sampleSheetFile)
-            runName = runInfo.containsKey("Sequence") ? runInfo.get("Sequence").getOrDefault("Name", YAML_SAMPLE_SHEET + " run") : YAML_SAMPLE_SHEET + " run"
+            try {
+                runInfo = YamlParser.parseFile(sampleSheetFile)
+                runName = runInfo.containsKey("Sequence") ? runInfo.get("Sequence").getOrDefault("Name", YAML_SAMPLE_SHEET + " run") : YAML_SAMPLE_SHEET + " run"
+            }
+            catch(Exception e) {
+                errors.add("Error parsing sample sheet file: " + e.getMessage())
+            }
+        }
+        else {
+            errors.add("Unknown sample sheet format, file " + sampleSheetFile.getName() + " can not be parsed")
         }
 
         def fileData = IOUtils.toString(sampleSheetItem.getInputStream(), StandardCharsets.UTF_8);
@@ -244,68 +255,74 @@ public class LoadProject extends GOperationSupport {
 
         String sampleFileName = sampleSheetFile.getName();
         String sampleFileExt = sampleFileName.replaceFirst(~/^([^\.])+/, '')
-        long fileType = getFileType(sampleFileExt)
-        if(fileType != 0)
-            def fileId = database.file_info << [fileName: sampleFileName,  fileType: fileType,
-                path: proj.getCompletePath().getChildPath(sampleFileName).toString(), size: sampleSheetFile.length(), project: projectId,
-                entity: "run", entityID: runId]
+        long fileType = getFileType(sampleSheetFile, sampleFileExt)
+        def fileId = database.file_info << [fileName: sampleFileName,  fileType: fileType,
+            path: proj.getCompletePath().getChildPath(sampleFileName).toString(), size: sampleSheetFile.length(), project: projectId,
+            entity: "run", entityID: runId]
         //samples
         //first row - column names
         List<List<String>> samplesData = sampleSheetFormat.equals(ILLUMINA_SAMPLE_SHEET ) ? runInfo.get("Data" ) : runInfo.get("Samples");
-        Map<String, Integer> nameToIndex = new HashMap<>();
-        List<String> names = samplesData.get(0);
-        for(int i = 0; i < names.size(); i++) {
-            nameToIndex.put(names.get(i ), i );
-        }
-        def runSampleFields = [:]
-        runSampleFields["plate"] = "Sample_ID"
-        runSampleFields["well"] = "Sample_Well"
-        runSampleFields["index_plate_well"] = "Index_Plate_Well"
-        runSampleFields["I7_index_id"] = "I7_Index_ID"
-        runSampleFields["index1"] = "index"
-        runSampleFields["I5_index_id"] = "I5_Index_ID"
-        runSampleFields["index2"] = "index2"
-        for(int i = 1; i < samplesData.size(); i++) {
-            List<String> sampleValues = samplesData.get(i);
-            String sampleName = getOrDefault(sampleValues, nameToIndex, "Sample_ID" );
-            long sampleTypeId  = Integer.valueOf(params.$sampleType)
-            def sampleId = database.samples << [project: projectId, typeID: sampleTypeId, title: sampleName, description: getOrDefault(sampleValues, nameToIndex, "Description" ) ]
+        if(samplesData!= null && !samplesData.isEmpty()) {
+            Map<String, Integer> nameToIndex = new HashMap<>();
+            List<String> names = samplesData.get(0);
+            for(int i = 0; i < names.size(); i++) {
+                nameToIndex.put(names.get(i ), i );
+            }
+            def runSampleFields = [:]
+            runSampleFields["plate"] = "Sample_ID"
+            runSampleFields["well"] = "Sample_Well"
+            runSampleFields["index_plate_well"] = "Index_Plate_Well"
+            runSampleFields["I7_index_id"] = "I7_Index_ID"
+            runSampleFields["index1"] = "index"
+            runSampleFields["I5_index_id"] = "I5_Index_ID"
+            runSampleFields["index2"] = "index2"
+            for(int i = 1; i < samplesData.size(); i++) {
+                List<String> sampleValues = samplesData.get(i);
+                String sampleName = getOrDefault(sampleValues, nameToIndex, "Sample_ID" );
+                long sampleTypeId  = Integer.valueOf(params.$sampleType)
+                def sampleId = database.samples << [project: projectId, typeID: sampleTypeId, title: sampleName, description: getOrDefault(sampleValues, nameToIndex, "Description" ) ]
 
 
-            def runSample = [:].withDefault{key -> return ""}
-            runSampleFields.each{ k, v -> runSample[k] = getOrDefault(sampleValues, nameToIndex, v )}
-            runSample["run_id"] = runId
-            runSample["sample_id"] = sampleId
-            def runSampleId = database.run_samples << runSample
-            if(params.$readsType.equals("single" )) {
-                if(namesNoExt.containsKey(sampleName)) {
-                    File fname = namesNoExt.get(sampleName);
-                    long fileTypeId = getFileType(fname.getName().replace(sampleName, ""))
-                    if(fileTypeId != 0)
-                        def fileId = database.file_info << [fileName: fname.name,  fileType: fileTypeId, path: samples.getCompletePath().getChildPath(fname.name).toString(), size: fname.length(), project: projectId, entity: "sample", entityID: sampleId]
+                def runSample = [:].withDefault{ key ->
+                    return ""
+                }
+                runSampleFields.each{ k, v ->
+                    runSample[k] = getOrDefault(sampleValues, nameToIndex, v )
+                }
+                runSample["run_id"] = runId
+                runSample["sample_id"] = sampleId
+                def runSampleId = database.run_samples << runSample
+                if(params.$readsType.equals("single" )) {
+                    if(namesNoExt.containsKey(sampleName)) {
+                        File fname = namesNoExt.get(sampleName);
+                        long fileTypeId = getFileType(fname, fname.getName().replace(sampleName, ""))
+                        fileId = database.file_info << [fileName: fname.name,  fileType: fileTypeId, path: samples.getCompletePath().getChildPath(fname.name).toString(), size: fname.length(), project: projectId, entity: "sample", entityID: sampleId]
+                    }
+                }
+                else if(params.$readsType.equals("paired" )) {
+                    def readName = sampleName + "_r1";
+                    if(namesNoExt.containsKey(readName)) {
+                        File fname = namesNoExt.get(readName);
+                        long fileTypeId = getFileType(fname, fname.getName().replace(sampleName, ""))
+                        fileId = database.file_info << [fileName: fname.name,  fileType: fileTypeId, path: samples.getCompletePath().getChildPath(fname.name).toString(), size: fname.length(), project: projectId, entity: "sample", entityID: sampleId]
+                    }
+                    readName = sampleName + "_r2";
+                    if(namesNoExt.containsKey(readName)) {
+                        File fname = namesNoExt.get(readName);
+                        long fileTypeId = getFileType(fname, fname.getName().replace(sampleName, ""))
+                        fileId = database.file_info << [fileName: fname.name,  fileType: fileTypeId, path: samples.getCompletePath().getChildPath(fname.name).toString(), size: fname.length(), project: projectId, entity: "sample", entityID: sampleId]
+                    }
                 }
             }
-            else if(params.$readsType.equals("paired" )) {
-                def readName = sampleName + "_r1";
-                if(namesNoExt.containsKey(readName)) {
-                    File fname = namesNoExt.get(readName);
-                    long fileTypeId = getFileType(fname.getName().replace(sampleName, ""))
-                    if(fileTypeId != 0)
-                        def fileId = database.file_info << [fileName: fname.name,  fileType: fileTypeId, path: samples.getCompletePath().getChildPath(fname.name).toString(), size: fname.length(), project: projectId, entity: "sample", entityID: sampleId]
-                }
-                readName = sampleName + "_r2";
-                if(namesNoExt.containsKey(readName)) {
-                    File fname = namesNoExt.get(readName);
-                    long fileTypeId = getFileType(fname.getName().replace(sampleName, ""))
-                    if(fileTypeId != 0)
-                        def fileId = database.file_info << [fileName: fname.name,  fileType: fileTypeId, path: samples.getCompletePath().getChildPath(fname.name).toString(), size: fname.length(), project: projectId, entity: "sample", entityID: sampleId]
-                }
-            }
+        }
+        else {
+            errors.add("Sample data is empty or not valid, no samples information was loaded" )
         }
 
-
-
-        setResult(OperationResult.finished())
+        if(errors.isEmpty())
+            setResult(OperationResult.finished())
+        else
+            OperationResult.finished(String.join("\n", errors))
     }
 
     def String getOrDefault(List<String> values, Map<String, Integer> keyToIndex, String key) {
@@ -326,13 +343,23 @@ public class LoadProject extends GOperationSupport {
         return ( projID != null );
     }
 
-    //TODO!!!!!
-    def long getFileType(String suffix) {
-        def typeID = db.oneLong( "SELECT id FROM file_types prj WHERE suffix = ?", suffix)
+    def long getFileType(File file, String fileExt) {
+
+        def typeID = db.oneLong( "SELECT id FROM file_types prj WHERE suffix = ?", fileExt)
         if(typeID != null)
             return Long.valueOf(typeID)
-        else
-            return 0
+        def genericType = ".binary"
+        if(FileTypeRegistry.isTextFile(file)) {
+            genericType = ".text"
+        }
+        typeID = db.oneLong( "SELECT id FROM file_types prj WHERE suffix = ?", genericType)
+        if(typeID != null)
+            return typeID
+        else {
+            def descr = genericType.equals(".text" )?"Generic text file": "Generic binary file"
+            typeID = database.file_types << [suffix: genericType, description: descr]
+            return typeID
+        }
     }
 
     def long getSampleType(String name) {
